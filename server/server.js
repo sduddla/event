f44e45a710cb9f5aebedc0706fd5945e944080eb
sduddla <sduddla921@gmail.com>
2025-12-24 02:30:10 +0900

    chore: 서버 설정 변경

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 import jsonServer from 'json-server'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const server = jsonServer.create()
-const router = jsonServer.router('./db.json')
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
